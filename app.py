854f5425daabab550ea55d32d43d0dc674b09368
fablegros-ux <fablegros@gmail.com>
2026-01-15 11:13:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ COLOR_MAP = {
     "rose": colors.HexColor("#E85D9E"),
     "vert": colors.HexColor("#2ECC71"),
     "jaune": colors.HexColor("#F1C40F"),
-    "blanc": colors,HexColor("#FFFFFF"),
+    "blanc": colors.HexColor("#FFFFFF"),
 }
 
 def pick_color_from_filename(filename: str) -> Tuple[str, colors.Color]:
